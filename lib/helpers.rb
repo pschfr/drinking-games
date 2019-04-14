e5f61b4afa744f9ac68b90b8b48b91da4caf9d22
Paul Schaefer <yeppaul2@gmail.com>
2019-04-14 16:43:14 -0400

    Added new post layout, enabled Blogging and LinkTo helpers, improved post loop

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,2 +1,8 @@
 # Enables partials by activating built-in rendering helper
 use_helper Nanoc::Helpers::Rendering
+
+# Enables blogging functionality
+use_helper Nanoc::Helpers::Blogging
+
+# Enables Rails-like link_to helper
+use_helper Nanoc::Helpers::LinkTo
